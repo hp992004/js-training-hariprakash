b9c4309087fca845023985cf5adb51bfa73a2eb8
hp992004 <hariprakash.i.a.2004@gmail.com>
2026-07-05 17:43:24 +0530

    nodejs/session1: complete activity tasks

---
+++ nodejs/session1/compare.js
@@ -0,0 +1,18 @@
+console.log("Node version:", process.version)
+console.log("Platform:", process.platform)
+
+/*
+console.log(document.querySelector("h1"))
+
+console.log(document.querySelector("h1"))
+            ^
+
+ReferenceError: document is not defined
+at Object.<anonymous> (D:\task1\js\js-training-hariprakash\nodejs\session1\compare.js:4:13)
+*/
+
+/*
+Node.js can work with files, use system information, and create web servers.
+It is mainly used for backend development and running JavaScript outside the browser.
+Browser JavaScript can interact with web pages using the document object, but Node.js cannot.
+*/
