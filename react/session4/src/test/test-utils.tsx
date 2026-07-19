8c8a96d69d7455e47ebfefe293ad669629bbb313
hp992004 <hariprakash.i.a.2004@gmail.com>
2026-07-19 15:28:32 +0530

    react/unit-testing: complete activity tasks

--- react/session4/src/test/test-utils.tsx
+++ react/session4/src/test/test-utils.tsx
@@ -1,19 +1,18 @@
+
 import type { ReactNode } from 'react'
-import { render} from '@testing-library/react'
-import type {  RenderOptions } from '@testing-library/react'
+import { render as rtlRender } from '@testing-library/react'
+import type { RenderOptions } from '@testing-library/react'
 import { ThemeProvider } from '../contexts/theme-context'
+import { InternProvider } from '../contexts/intern-context'
 
 function AllProviders({ children }: { children: ReactNode }) {
-  return (
-    <ThemeProvider>
-      {children}
-    </ThemeProvider>
-  )
+  return <ThemeProvider><InternProvider>{children}</InternProvider></ThemeProvider>
 }
 
 function customRender(ui: React.ReactElement, options?: RenderOptions) {
-  return render(ui, { wrapper: AllProviders, ...options })
+  return rtlRender(ui, { wrapper: AllProviders, ...options })
 }
-
 export * from '@testing-library/react'
 export { customRender as render }
+
+
